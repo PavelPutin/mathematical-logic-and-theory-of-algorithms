dcb645a210be8da6c511425420aca28be53df87e
Putin Pavel <putin@cs.vsu.ru>
2022-04-23 10:59:03 +0300

    Добавил возможность решать систему линейных уравнений методом обратной матрицы

---
+++ src/pavel_p_a/system_of_equations/InverseMatrixMethod.java
@@ -0,0 +1,27 @@
+package pavel_p_a.system_of_equations;
+
+import pavel_p_a.matrix.MatrixCalculator;
+
+public class InverseMatrixMethod {
+    public static double[] solve(double[][] coefficients, double[] freeTerms) throws Exception {
+        if (!MatrixCalculator.isSquareMatrix(coefficients)) {
+            throw new Exception("Метод Крамера применим только тогда, когда число неизвестных равно числу уравнений!");
+        }
+
+        if (coefficients.length != freeTerms.length) {
+            throw new Exception("Не совпадает количество свободных членов и уравнений!");
+        }
+
+        double delta = MatrixCalculator.calcDeterminant(coefficients);
+
+        if (delta == 0) {
+            throw new Exception("Определитель матрицы коэффициентов равен нулю. Невозможно решить методом обратной матрицы!");
+        }
+
+        double[] result = MatrixCalculator.multiply(
+                MatrixCalculator.getInverseMatrix(coefficients), freeTerms
+        );
+
+        return result;
+    }
+}
