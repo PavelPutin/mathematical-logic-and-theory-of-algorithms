bee8bc0ab4164546cfe8d2f4360f0bbfbf862232
Putin Pavel <putin@cs.vsu.ru>
2022-04-24 15:06:35 +0300

    Удалил ненужные файлы старой версии проекта

--- src/pavel_p_a/system_of_equations/SystemOfEquations.java
+++
@@ -1,8 +0,0 @@
-package pavel_p_a.system_of_equations;
-
-import pavel_p_a.matrix.Matrix;
-
-public interface SystemOfEquations {
-    Matrix getCoefficientsMatrix();
-    Matrix getFreeTermMatrix();
-}
