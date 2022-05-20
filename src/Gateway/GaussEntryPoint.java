7a56bb299372458d7d4ce56c5840a4d01fd25e0f
Putin Pavel <pavelputin2003@yandex.ru>
2022-05-20 20:03:52 +0300

    Make gateway for java

--- src/Gateway/GaussEntryPoint.java
+++ src/Gateway/GaussEntryPoint.java
@@ -1,22 +1,24 @@
 package Gateway;
 
-import java.util.List;
-import java.util.Map;
+import pavel_p_a.matrix.MatrixCalculator;
+import pavel_p_a.system_of_equations.GaussMethod;
+
+import java.util.*;
 
 public class GaussEntryPoint {
     /**
      * Ответ java "сервера"
      */
     public static class Response { // выходные данные напиши такие, которые будут тебе удобны
-        private Map<Integer, List<List<Double>>> eVectors;
+        private Map<Double, List<List<Double>>> eVectors;
 
         // можно опциально передать: код выполнения/текст ошибки
 
-        public Response(Map<Integer, List<List<Double>>> eVectors) {
+        public Response(Map<Double, List<List<Double>>> eVectors) {
             this.eVectors = eVectors;
         }
 
-        public Map<Integer, List<List<Double>>> getEVectors() {
+        public Map<Double, List<List<Double>>> getEVectors() {
             return eVectors;
         }
     }
@@ -27,9 +29,32 @@ public class GaussEntryPoint {
      * @param matrix
      * @param vals   собственные значения и их кратности
      */
-    public Response getResponseForMatrix(Double[][] matrix, Map<Double, Integer> vals) {
+    public Response getResponseForMatrix(Double[][] matrix, Map<Double, Integer> vals) throws Exception {
         // тут вызов гаусса и создание класса ответа
-        Response response = null; /* = new Response(...);*/
+        Map<Double, List<List<Double>>> eVectors = new TreeMap<>();
+        for (Double val : vals.keySet()) {
+            int number = vals.get(val);
+            double[][] nm = new double[matrix.length][matrix[0].length];
+            for (int r = 0; r < matrix.length; r++) {
+                for (int c = 0; c < matrix[0].length; c++) {
+                    nm[r][c] = (r == c) ? matrix[r][c] - val : matrix[r][c];
+                }
+            }
+            int rank = MatrixCalculator.calcRank(nm);
+            int s = nm.length - rank;
+            double[] ft = new double[nm.length];
+            double[][] result = GaussMethod.solve(nm, ft, s);
+
+            eVectors.put(val, new ArrayList<>());
+            for (int i = 0; i < s; i++) {
+                List<Double> vector = new ArrayList<>();
+                for (int j = 0; j < result[0].length; j++) {
+                    vector.add(result[i][j]);
+                }
+                eVectors.get(val).add(vector);
+            }
+        }
+        Response response = new Response(eVectors);
 
         return response;
     }
